c2ddc3a7274054a7f227f078631e9e331cc73f10
mdswoponsheik <mdswoponshek1226@gmail.com>
2026-04-15 04:04:07 +0600

    added navbar and footer

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h2 className='h-200'>Welcome to the Home Page</h2>
+      <h2 className='h-201'>Welcome to the Home Page</h2>
     </div>
   )
 }
